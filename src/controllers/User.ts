f8491c46701aa5c535238ece097669ba1c2cee12
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2022-09-09 16:34:52 -0300

    feat: userController deve retonar status 400 quando email não é informado

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express'
 export default class UserController {
   async add (req: Request, res: Response): Promise<Response> {
     if (!req.body.email) {
-      return res.status(400).json({ message: 'Email não informado' })
+      return res.status(400).json({ message: 'Email não informado.' })
     }
     return res.status(200)
   }
--- test/integration/User.test.ts
+++ test/integration/User.test.ts
@@ -4,8 +4,8 @@ import app from '../../src/app'
 
 describe('UserController', () => {
   describe('#add', () => {
-    describe('quando o email não é informado', () => {
-      it('deve retornar um status 400', async () => {
+    describe('quando o campo email não é informado', () => {
+      it('deve retornar um status 400.', async () => {
         const response = await supertest(app)
           .post('/user')
           .send({ username: 'any_username', password: 'any_password' })
