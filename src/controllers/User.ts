7a0448a0f15d1796dc5f660150ebdbc380f01604
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2022-09-09 17:04:07 -0300

    refactor: remove codigo duplicado

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -2,12 +2,11 @@ import { Request, Response } from 'express'
 
 export default class UserController {
   async add (req: Request, res: Response): Promise<Response> {
-    if (!req.body.email) {
-      return res.status(400).json({ message: 'Email não informado' })
-    }
-
-    if (!req.body.password) {
-      return res.status(400).json({ message: 'Password não informado' })
+    const requiredFields = ['email', 'password']
+    for (const field of requiredFields) {
+      if (!req.body[field]) {
+        return res.status(400).json({ message: `${field} não informado` })
+      }
     }
 
     return res.status(200).json()
