52fb0ad6775c9326fa01e4341c62e67c927bca05
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2022-09-09 17:05:36 -0300

    feat: userController deve retonar status 400 quando username não é informado

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 
 export default class UserController {
   async add (req: Request, res: Response): Promise<Response> {
-    const requiredFields = ['email', 'password']
+    const requiredFields = ['email', 'password', 'username']
     for (const field of requiredFields) {
       if (!req.body[field]) {
         return res.status(400).json({ message: `${field} não informado` })
